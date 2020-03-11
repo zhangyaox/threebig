6da7d3176c625423fba429c843a1c99acf5c4859
zhangyaox <1336259143@qq.com>
2020-03-11 21:18:14 +0800

    添加3个子项目2

--- hgshop-interfaces/src/main/java/com/zhangyaoxing/service/UserService.java
+++
@@ -1,5 +0,0 @@
-package com.zhangyaoxing.service;
-
-public interface UserService {
-	boolean login(String name,String pass);//登录功能
-}
--- hgshop-service/src/main/java/com/zhangyaoxing/mapper/UserMapper.java
+++
@@ -1,5 +0,0 @@
-package com.zhangyaoxing.mapper;
-
-public class UserMapper {
-
-}
